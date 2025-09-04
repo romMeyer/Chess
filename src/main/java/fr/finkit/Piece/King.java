cf11e387302570929224a8ccac0bf0ea8c87cf22
romMeyer <romain.meyer.fkpt@gmail.com>
2025-09-04 09:59:19 +0200

    king movement

--- src/main/java/fr/finkit/Piece/King.java
+++ src/main/java/fr/finkit/Piece/King.java
@@ -2,6 +2,7 @@ package fr.finkit.Piece;
 
 import fr.finkit.core.Board;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class King extends Piece {
@@ -11,8 +12,30 @@ public class King extends Piece {
     }
 
     @Override
-    public List<Position> getLegalMoves(Board board, Position position) {
-        return List.of();
+    public List<Position> getLegalMoves(Board board, Position pos) {
+        List<Position> moves = new ArrayList<>();
+        List<Position> legalMoves = new ArrayList<>();
+
+        moves.add(new Position(pos.x()+1, pos.y()));
+        moves.add(new Position(pos.x(), pos.y()+1));
+        moves.add(new Position(pos.x()+1, pos.y()+1));
+
+        moves.add(new Position(pos.x()-1, pos.y()));
+        moves.add(new Position(pos.x(), pos.y()-1));
+        moves.add(new Position(pos.x()-1, pos.y()-1));
+
+        moves.add(new Position(pos.x()-1, pos.y()+1));
+        moves.add(new Position(pos.x()+1, pos.y()-1));
+
+        int x, y;
+        for (Position p : moves) {
+            x = p.x();
+            y = p.y();
+            if (( x <= 7 && x >= 0 ) && (y <= 7 && y >= 0))
+                if (board.getPiece(p) == null || board.getPiece(p).getColor() != pieceColor)
+                    legalMoves.add(p);
+        }
+        return legalMoves;
     }
 
 }
