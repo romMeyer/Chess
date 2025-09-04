ccba35509f97a8368b2fc6ae15d827a7e01c96d4
romMeyer <romain.meyer.fkpt@gmail.com>
2025-09-04 11:08:05 +0200

    Rook movement

--- src/main/java/fr/finkit/Piece/Bishop.java
+++ src/main/java/fr/finkit/Piece/Bishop.java
@@ -13,58 +13,41 @@ public class Bishop extends Piece {
 
     @Override
     public List<Position> getLegalMoves(Board board, Position pos) {
-        List<Position> moves = new ArrayList<>();
         List<Position> legalMoves = new ArrayList<>();
         int x, y;
 
         // Diagonale bas-droite
         for (x = pos.x() + 1, y = pos.y() + 1; x <= 7 && y <= 7; x++, y++) {
-            Position p = new Position(x, y);
-            Piece piece = board.getPiece(p);
-            if (piece == null) {
-                legalMoves.add(p);
-            } else {
-                if (piece.getColor() != this.pieceColor) legalMoves.add(p);
-                break;
-            }
+            if (diagonal(board, legalMoves, x, y)) break;
         }
 
         // Diagonale haut-droite
         for (x = pos.x() + 1, y = pos.y() - 1; x <= 7 && y >= 0; x++, y--) {
-            Position p = new Position(x, y);
-            Piece piece = board.getPiece(p);
-            if (piece == null) {
-                legalMoves.add(p);
-            } else {
-                if (piece.getColor() != this.pieceColor) legalMoves.add(p);
-                break;
-            }
+            if (diagonal(board, legalMoves, x, y)) break;
         }
 
         // Diagonale bas-gauche
         for (x = pos.x() - 1, y = pos.y() + 1; x >= 0 && y <= 7; x--, y++) {
-            Position p = new Position(x, y);
-            Piece piece = board.getPiece(p);
-            if (piece == null) {
-                legalMoves.add(p);
-            } else {
-                if (piece.getColor() != this.pieceColor) legalMoves.add(p);
-                break;
-            }
+            if (diagonal(board, legalMoves, x, y)) break;
         }
 
         // Diagonale haut-gauche
         for (x = pos.x() - 1, y = pos.y() - 1; x >= 0 && y >= 0; x--, y--) {
-            Position p = new Position(x, y);
-            Piece piece = board.getPiece(p);
-            if (piece == null) {
-                legalMoves.add(p);
-            } else {
-                if (piece.getColor() != this.pieceColor) legalMoves.add(p);
-                break;
-            }
+            if (diagonal(board, legalMoves, x, y)) break;
         }
         return legalMoves;
+    }
+
+    private boolean diagonal(Board board, List<Position> legalMoves, int x, int y) {
+        Position p = new Position(x, y);
+        Piece piece = board.getPiece(p);
+        if (piece == null) {
+            legalMoves.add(p);
+        } else {
+            if (piece.getColor() != pieceColor) legalMoves.add(p);
+            return true;
+        }
+        return false;
     }
 
 }
--- src/main/java/fr/finkit/Piece/Rook.java
+++ src/main/java/fr/finkit/Piece/Rook.java
@@ -2,6 +2,7 @@ package fr.finkit.Piece;
 
 import fr.finkit.core.Board;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Rook extends Piece {
@@ -11,8 +12,59 @@ public class Rook extends Piece {
     }
 
     @Override
-    public List<Position> getLegalMoves(Board board, Position position) {
-        return List.of();
+    public List<Position> getLegalMoves(Board board, Position pos) {
+        List<Position> legalMoves = new ArrayList<>();
+        int x, y;
+
+        // Ligne bas
+        for (x = pos.x()+1 ; x <= 7; x++) {
+            if (vertical(board, pos, legalMoves, x)) break;
+        }
+
+        // Ligne haut
+        for (x = pos.x()-1 ; x >= 0; x--) {
+            if (vertical(board, pos, legalMoves, x)) break;
+        }
+
+        // Ligne droite
+        for (y = pos.y()+1 ; y <= 7; y++) {
+            if (horizontal(board, pos, legalMoves, y)) break;
+        }
+
+        // Ligne gauche
+        for (y = pos.y()-1 ; y >= 0; y--) {
+            if (horizontal(board, pos, legalMoves, y)) break;
+        }
+
+        return legalMoves;
+    }
+
+    private boolean vertical(Board board, Position pos, List<Position> legalMoves, int x) {
+        Position p;
+        Piece piece;
+        p = new Position(x, pos.y());
+        piece = board.getPiece(p);
+        if (piece == null) {
+            legalMoves.add(p);
+        } else {
+            if (piece.getColor() != pieceColor) legalMoves.add(p);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean horizontal(Board board, Position pos, List<Position> legalMoves, int y) {
+        Position p;
+        Piece piece;
+        p = new Position(pos.x(), y);
+        piece = board.getPiece(p);
+        if (piece == null) {
+            legalMoves.add(p);
+        } else {
+            if (piece.getColor() != pieceColor) legalMoves.add(p);
+            return true;
+        }
+        return false;
     }
 
 }
