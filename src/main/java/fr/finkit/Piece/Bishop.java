5094d5abefd5ee8ad1ffaf21c10512a3aa1d02dd
romMeyer <romain.meyer.fkpt@gmail.com>
2025-09-04 09:59:35 +0200

    bishop movement

--- src/main/java/fr/finkit/Piece/Bishop.java
+++ src/main/java/fr/finkit/Piece/Bishop.java
@@ -2,6 +2,7 @@ package fr.finkit.Piece;
 
 import fr.finkit.core.Board;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bishop extends Piece {
@@ -11,8 +12,26 @@ public class Bishop extends Piece {
     }
 
     @Override
-    public List<Position> getLegalMoves(Board board, Position position) {
-        return List.of();
+    public List<Position> getLegalMoves(Board board, Position pos) {
+        List<Position> moves = new ArrayList<>();
+        List<Position> legalMoves = new ArrayList<>();
+
+        for (int i = 0; i < 7; i++) {
+            moves.add(new Position(pos.x()+i, pos.y()+i));
+            moves.add(new Position(pos.x()-i, pos.y()+i));
+            moves.add(new Position(pos.x()+i, pos.y()-i));
+            moves.add(new Position(pos.x()-i, pos.y()-i));
+        }
+        int x, y;
+        for (Position p : moves) {
+            x = p.x();
+            y = p.y();
+            if (( x <= 7 && x >= 0 ) && (y <= 7 && y >= 0))
+                if (board.getPiece(p) == null || board.getPiece(p).getColor() != pieceColor)
+                    legalMoves.add(p);
+        }
+
+        return legalMoves;
     }
 
 }
